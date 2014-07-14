69cc353362b7ee0f5eac9d609c97748b8af1edb1
Stijn <stijnc@fooji.net>
2014-07-14 13:12:04 +0200

    Create Send-sSMS.ps1

---
+++ BizzSMS/Exmaple/Send-sSMS.ps1
@@ -0,0 +1,33 @@
+<#
+.NOTES
+  this runbook calls the runbook Send-SMS
+  depended on the BizzSMSConnection automation variable
+#>
+
+Workflow Send-sSMS {
+
+  param(
+    
+    [Parameter(mandatory=$true)]
+    [Alias('Message')]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({$_.lenght -lt 100})]
+    [string]$Text,
+
+    [Parameter(mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidatePattern('^((\+|00)32\s?|0)4(60|[789]\d)(\s?\d{2}){3}$')]
+    [string[]]$Phonenumber
+  )
+
+    #get some variables
+    $connection = get-automationConnection -Name 'BizzSMSConnection'
+    $props = @{
+      $username = $connection.username
+      $apikey = $connection.apikey
+    }
+
+    $Numbers = $Phonenumber -join ','
+
+    Send-SMS -phonenumber $Numbers -text $Text @props
+}
